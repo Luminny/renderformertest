1bd9efd3285fb726e79c095abdcdb9c49af91e09
MIHOYO\lumin.yang <lumin.yang@mihoyo.com>
2025-07-01 17:46:24 +0800

    ,

--- renderformer/__init__.py
+++ renderformer/__init__.py
@@ -1,4 +1,6 @@
 from renderformer.models.renderformer import RenderFormer
+from renderformer.models.geo_raster import GeoRaster
 from renderformer.pipelines.rendering_pipeline import RenderFormerRenderingPipeline
+from renderformer.pipelines.geo_raster_pipeline import GeoRasterRenderingPipeline
 
-__all__ = ['RenderFormerRenderingPipeline', 'RenderFormer']
+__all__ = ['RenderFormerRenderingPipeline', 'RenderFormer', 'GeoRasterRenderingPipeline', 'GeoRaster']
